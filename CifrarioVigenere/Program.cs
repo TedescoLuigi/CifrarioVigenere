c1408fd675359bec75102f84bc5b9af563c7c6bc
luigitedesco <luigitedesco@LUIGITEDESCB974>
2026-02-15 09:47:35 +0100

    finito

--- CifrarioVigenere/Program.cs
+++ CifrarioVigenere/Program.cs
@@ -9,21 +9,84 @@ namespace CifrarioVigenere
             Console.WriteLine("ESERCIZIO CIFRARIO VIGENERE");
             Console.WriteLine("");
 
-            Console.WriteLine("Inserisci una frase da cifrare");
-            string parola = Console.ReadLine();
+            Console.WriteLine("Inserisci una frase da cifrare:");
+            string frase = Console.ReadLine().ToLower();
 
-            Console.WriteLine("inserisci parola k");
-            string codice= Console.ReadLine();
+            //  Rimuovo gli spazi con un for
+            string fraseSenzaSpazi = "";
 
-            Console.WriteLine("Premi un tasto per avviare la cifratura");
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if (frase[i] != ' ')
+                {
+                    fraseSenzaSpazi = fraseSenzaSpazi + frase[i];
+                }
+            }
+
+            frase = fraseSenzaSpazi;
+
+            Console.WriteLine("Inserisci la parola chiave:");
+            string chiave = Console.ReadLine().ToLower();
+
+            // Allungo la chiave fino alla lunghezza della frase
+            string chiaveEstesa = "";
+            int indice = 0;
+
+            for (int i = 0; i < frase.Length; i++)
+            {
+                chiaveEstesa = chiaveEstesa + chiave[indice];
+
+                indice++;
+
+                if (indice == chiave.Length)
+                {
+                    indice = 0;
+                }
+            }
+
+            char[] alfabeto = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+
+            //  Cifratura
+            string risultato = "";
+
+            for (int i = 0; i < frase.Length; i++)
+            {
+                int posizioneFrase = 0;
+                int posizioneChiave = 0;
+
+                // Trovo posizione lettera frase
+                for (int j = 0; j < 26; j++)
+                {
+                    if (alfabeto[j] == frase[i])
+                    {
+                        posizioneFrase = j;
+                    }
+                }
+
+                // Trovo posizione lettera chiave estesa
+                for (int j = 0; j < 26; j++)
+                {
+                    if (alfabeto[j] == chiaveEstesa[i])
+                    {
+                        posizioneChiave = j;
+                    }
+                }
+
+                int nuovaPosizione = posizioneFrase + posizioneChiave;
+
+                if (nuovaPosizione >= 26)
+                {
+                    nuovaPosizione = nuovaPosizione - 26;
+                }
+
+                risultato = risultato + alfabeto[nuovaPosizione];
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Chiave estesa: " + chiaveEstesa);
+            Console.WriteLine("Frase cifrata: " + risultato);
+
             Console.ReadLine();
-
-            char[] Arrayparola = parola.ToCharArray();
-
-            char[] alfabeto = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-
-  
-
         }
     }
 }
